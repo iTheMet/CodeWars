efec029f763b8c46afd361483457b41db40f555d
iTheMet <44820844+iTheMet@users.noreply.github.com>
2021-05-02 00:43:46 +0300

    шш

--- CW/src/main/java/isogram.java
+++ CW/src/main/java/isogram.java
@@ -1,8 +1,49 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class isogram {
+    public static String str = new String();
+    public static String  printing() {
 
-    public static boolean  isIsogram(String str) {
-
-        str = system.out.;
+      Scanner scanner = new Scanner(System.in);
+      str = scanner.nextLine();
+      str = str.toUpperCase();
+      System.out.println(str);
+      return str;
     }
 
+    public static boolean proverka(String pr){
+        pr = pr.toUpperCase();
+        boolean gate = true;
+        char commonLetter;
+        char suspectLetter;
+        for(int i=0; i < pr.length();i++ ){
+
+            if(i+1 != pr.length() && gate != false){
+                commonLetter = pr.charAt(i);
+                System.out.println("________________________________\nнаша буква '"+commonLetter+"'");
+                for(int j = i+1; j< pr.length();j++){
+                    suspectLetter = pr.charAt(j);
+                    System.out.println("проверка буквы '"+suspectLetter+"'");
+                    if(commonLetter == suspectLetter){
+                        gate = false;
+                        break;
+                    }
+                }
+            }else{
+                System.out.println(gate);
+                break;
+            }
+        }
+        return gate;
+    }
+
+    public static boolean  isIsogram(String pr) {
+        return pr.length() == pr.toLowerCase().chars().distinct().count();
+
+    }
+    public static long erg(){
+        int sas[] = {2,4,5};
+        return Arrays.stream(sas).count();
+    }
 }
--- CW/src/main/java/main.java
+++ CW/src/main/java/main.java
@@ -1,8 +1,9 @@
-public class main {
+public class Main {
 
     public static void main(String args[]){
-
-
+//isogram.proverka(isogram.printing());
+//System.out.println(isogram.isIsogram(isogram.printing()));
+        //System.out.println((isogram.erg()));
 
        return ;
 
